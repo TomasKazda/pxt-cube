23cc27596d531061d97bd0b055d7be2c59484cce
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2021-10-21 11:24:34 +0200

    cube with events

--- main.py
+++ main.py
@@ -1,14 +1,114 @@
-povoleno = False
+def ThrowEnable():
+    global throwEnabled
+    basic.show_icon(IconNames.YES)
+    throwEnabled = True
+def SideChange():
+    global kostka
+    if kostka == 10:
+        kostka = 6
+    else:
+        kostka = 10
+    basic.show_number(kostka)
+def Throwing():
+    global throwEnabled
+    if throwEnabled:
+        throwEnabled = False
+        hod = randint(1, kostka)
+        if hod == 1:
+            basic.show_leds("""
+                . . . . .
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                . . . . .
+            """)
+        if hod == 2:
+            basic.show_leds("""
+                # . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . #
+            """)
+        if hod == 3:
+            basic.show_leds("""
+                # . . . .
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                . . . . #
+            """)
+        if hod == 4:
+            basic.show_leds("""
+                # . . . #
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                # . . . #
+            """)
+        if hod == 5:
+            basic.show_leds("""
+                # . . . #
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                # . . . #
+            """)
+        if hod == 6:
+            basic.show_leds("""
+                # . . . #
+                                . . . . .
+                                # . . . #
+                                . . . . .
+                                # . . . #
+            """)
+        if hod == 7:
+            basic.show_leds("""
+                # . . . #
+                                . . . . .
+                                # . # . #
+                                . . . . .
+                                # . . . #
+            """)
+        if hod == 8:
+            basic.show_leds("""
+                # . . . #
+                                . . # . .
+                                . # . # .
+                                . . # . .
+                                # . . . #
+            """)
+        if hod == 9:
+            basic.show_leds("""
+                # . . . #
+                                . . # . .
+                                . # # # .
+                                . . # . .
+                                # . . . #
+            """)
+        if hod == 10:
+            basic.show_leds("""
+                # . . . #
+                                . # . # .
+                                # . . . #
+                                . # . # .
+                                # . . . #
+            """)
+        for index in range(hod):
+            music.play_tone(262, music.beat(4))
+            music.rest(music.beat(2))
 
-def on_button_pressed_a():
-    global povoleno
-    povoleno = True
-input.on_button_pressed(Button.A, on_button_pressed_a)
 
+
+kostka = 0
+throwEnabled = True
+kostka = 10
+
+input.on_button_pressed(Button.A, ThrowEnable)
+input.on_logo_event(TouchButtonEvent.PRESSED, SideChange)
+input.on_gesture(Gesture.SHAKE, Throwing)
 
 def on_forever():
-    if povoleno:
-        basic.show_icon(IconNames.YES)
-    else:
-        basic.show_icon(IconNames.NO)
+    pass
 basic.forever(on_forever)
+
--- main.ts
+++ main.ts
@@ -1,13 +1,134 @@
-let povoleno = false
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+let kostka = 0
+let throwEnabled = true
+kostka = 10
+input.onButtonPressed(Button.A, function ThrowEnable() {
     
-    povoleno = true
+    basic.showIcon(IconNames.Yes)
+    throwEnabled = true
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function SideChange() {
+    
+    if (kostka == 10) {
+        kostka = 6
+    } else {
+        kostka = 10
+    }
+    
+    basic.showNumber(kostka)
+})
+input.onGesture(Gesture.Shake, function Throwing() {
+    let hod: number;
+    
+    if (throwEnabled) {
+        throwEnabled = false
+        hod = randint(1, kostka)
+        if (hod == 1) {
+            basic.showLeds(`
+                . . . . .
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                . . . . .
+            `)
+        }
+        
+        if (hod == 2) {
+            basic.showLeds(`
+                # . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . #
+            `)
+        }
+        
+        if (hod == 3) {
+            basic.showLeds(`
+                # . . . .
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                . . . . #
+            `)
+        }
+        
+        if (hod == 4) {
+            basic.showLeds(`
+                # . . . #
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 5) {
+            basic.showLeds(`
+                # . . . #
+                                . . . . .
+                                . . # . .
+                                . . . . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 6) {
+            basic.showLeds(`
+                # . . . #
+                                . . . . .
+                                # . . . #
+                                . . . . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 7) {
+            basic.showLeds(`
+                # . . . #
+                                . . . . .
+                                # . # . #
+                                . . . . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 8) {
+            basic.showLeds(`
+                # . . . #
+                                . . # . .
+                                . # . # .
+                                . . # . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 9) {
+            basic.showLeds(`
+                # . . . #
+                                . . # . .
+                                . # # # .
+                                . . # . .
+                                # . . . #
+            `)
+        }
+        
+        if (hod == 10) {
+            basic.showLeds(`
+                # . . . #
+                                . # . # .
+                                # . . . #
+                                . # . # .
+                                # . . . #
+            `)
+        }
+        
+        for (let index = 0; index < hod; index++) {
+            music.playTone(262, music.beat(4))
+            music.rest(music.beat(2))
+        }
+    }
+    
 })
 basic.forever(function on_forever() {
-    if (povoleno) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
     
 })
